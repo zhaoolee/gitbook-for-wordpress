a44e9fe0c37f8b1acbdad9f411b85673f54edb7b
zhaoolee <zhaoolee@gmail.com>
2023-03-26 16:37:09 +0800

    备案号更新

--- footer-container.php
+++ footer-container.php
@@ -1,6 +1,5 @@
 <div class='footer-container'>
     <footer class="site-footer">
-
         <div class="footer-widgets">
             <?php if (is_active_sidebar('footer-widgets')): // 检查是否有激活的页脚小工具 ?>
                 <?php dynamic_sidebar('footer-widgets'); // 显示页脚中的小工具区域 ?>
@@ -14,7 +13,7 @@
         </div>
         <h2 class="widget-title subheading heading-size-3">备案信息</h2>
         <div class="textwidget custom-html-widget">
-            <a href="https://beian.miit.gov.cn/" target="_blank" rel="noopener noreferrer">琼ICP备18002320号-1</a>
+            <a href="https://beian.miit.gov.cn/" target="_blank" rel="noopener noreferrer">琼ICP备18002320号-2</a>
         </div>
     </footer>
     <?php wp_footer(); // WordPress页脚钩子，用于加载JavaScript和其他元素 ?>
