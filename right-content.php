aa2d25971c941dd465f9805edeaed5b336bf77d4
zhaoolee <zhaoolee@gmail.com>
2023-04-01 14:19:26 +0800

    移动端侧边栏修复

--- right-content.php
+++ right-content.php
@@ -2,8 +2,8 @@
     <?php if (have_posts()): ?>
         <?php while (have_posts()):
             the_post(); ?>
-            <h2><?php the_title(); ?></h2> <!-- 在此处添加标题 -->
-            <?php the_content(); ?>
+            <div class="post-content"><h2><?php the_title(); ?></h2> <!-- 在此处添加标题 -->
+            <?php the_content(); ?></div>
             <?php comments_template(); ?>
         <?php endwhile; ?>
     <?php endif; ?>
