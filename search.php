815bda28d0dcf61849a91d2bca7635df240c8dee
zhaoo lee <zhaoolee@gmail.com>
2024-08-17 14:01:02 +0800

    update search page

--- search.php
+++ search.php
@@ -12,20 +12,40 @@
             </h1>
         </header>
 
-        <?php if (have_posts()):
-            while (have_posts()):
-                the_post(); ?>
+        <?php
+        $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+        $args = array(
+            's' => get_search_query(),
+            'posts_per_page' => 5,
+            'paged' => $paged
+        );
+        $search_query = new WP_Query($args);
+
+        if ($search_query->have_posts()):
+            while ($search_query->have_posts()):
+                $search_query->the_post(); ?>
                 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
                     <header class="entry-header">
-                        <h2 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
-                        </h2>
+                        <h2 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
                     </header>
 
                     <div class="entry-summary">
                         <?php the_excerpt(); ?>
                     </div>
                 </article>
-            <?php endwhile; else: ?>
+            <?php endwhile;
+            // Pagination
+            echo '<div class="pagination">';
+            echo paginate_links(array(
+                'total' => $search_query->max_num_pages,
+                'current' => $paged,
+                'prev_text' => __('&laquo; Previous'),
+                'next_text' => __('Next &raquo;'),
+            ));
+            echo '</div>';
+
+            wp_reset_postdata();
+        else: ?>
             <p>
                 <?php _e('Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'your_theme_text_domain'); ?>
             </p>
